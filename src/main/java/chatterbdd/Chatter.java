4588c8c6f081a99c02dc90de19c7a3557f24798f
iwser019 <vanok09@mail.ru>
2018-05-17 22:19:26 +0700

    Добавление метода для получения ответа

--- src/main/java/chatterbdd/Chatter.java
+++ src/main/java/chatterbdd/Chatter.java
@@ -1,4 +1,14 @@
 package chatterbdd;
 
 public class Chatter {
+
+    // Конструктор
+    public Chatter() {
+
+    }
+
+
+    public String getAnswer(String saying) {
+        return "Не понял.";
+    }
 }
