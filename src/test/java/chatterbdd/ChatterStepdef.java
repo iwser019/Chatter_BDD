f3025388cf2b039f9cfc99259d439de44dcd8650
iwser019 <vanok09@mail.ru>
2018-05-19 17:34:05 +0700

    Добавление новых определений (step definition)

--- src/test/java/chatterbdd/ChatterStepdef.java
+++ src/test/java/chatterbdd/ChatterStepdef.java
@@ -8,11 +8,15 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.junit.Assert;
 
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class ChatterStepdef {
 
     private Chatter chatter;
     private String saying;
     private String answer;
+    private HashMap<String, String> exMatchBase;
 
     @Given("^I have a chat program$")
     public void iHaveAChatProgram() throws Throwable {
@@ -45,4 +49,35 @@ public class ChatterStepdef {
         // Write code here that turns the phrase above into concrete actions
         Assert.assertEquals(arg0, this.answer);
     }
+
+    @When("^I need an exact match support$")
+    public void iNeedAnExactMatchSupport() throws Throwable {
+        this.exMatchBase = new HashMap<>();
+    }
+
+    @And("^I define an exact match like \"([^\"]*)\" with \"([^\"]*)\"$")
+    public void iDefineAnExactMatchLikeWith(String arg0, String arg1) throws Throwable {
+        exMatchBase.put(arg0, arg1);
+    }
+
+    @And("^I set these exact matches up$")
+    public void iSetTheseExactMatchesUp() throws Throwable {
+        chatter.setExactMatchBase(exMatchBase);
+    }
+
+    @Then("^The program should have the same exact matches as set before$")
+    public void theProgramShouldHaveTheSameExactMatchesAsSetBefore() throws Throwable {
+        HashMap exMatchBaseNew = chatter.getExactMatchBase();
+        boolean ok = true;
+        ok &= Arrays.equals(exMatchBaseNew.keySet().toArray(),
+                exMatchBase.keySet().toArray());
+        if (ok) {
+            for (String str : (String[]) exMatchBase.keySet().toArray()) {
+                ok &= exMatchBaseNew.get(str).equals(exMatchBase.get(str));
+            }
+        }
+        Assert.assertEquals(true, ok);
+    }
+
+
 }
